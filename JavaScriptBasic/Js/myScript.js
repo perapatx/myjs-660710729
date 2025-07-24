213917869df5e4874ca1193b9f3800ce1fd5828f
Perapat klaysamneang <klaysamneang_p@su.ac.th>
2025-07-24 04:08:28 +0000

    console-log1

--- JavaScriptBasic/Js/myScript.js
+++ JavaScriptBasic/Js/myScript.js
@@ -1,3 +1,11 @@
+let x = false
 function myFunction() {
+  if (x==false){
+
     document.getElementById("demo").innerHTML = "Paragraph changed.";
+    x=true
+  }else{
+    document.getElementById("demo").innerHTML = "Paragraph changed again";
+    x=false
   }
+}
